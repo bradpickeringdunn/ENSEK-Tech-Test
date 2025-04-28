7e491fb6491c0df18ade4b425c8d14c1174860d0
Brad Pickering-Dunn <brad@pickering-dunn.com>
2025-04-28 07:47:03 +0100

    Added upsert

--- ENSEK.Imports.Tests/Validators/CsvValidatorTests.cs
+++ ENSEK.Imports.Tests/Validators/CsvValidatorTests.cs
@@ -28,7 +28,6 @@ public class CsvValidatorTests
         Assert.AreEqual(result.Count, result.Distinct().Count());
     }
 
-
     [TestMethod]
     [DataRow("date")]
     [DataRow("10102010")]
@@ -44,5 +43,23 @@ public class CsvValidatorTests
 
         Assert.IsNotNull(result);
         Assert.IsTrue(result.All(x => x.Contains("as Meter Reading DateTime")));
+    }
+
+
+    [TestMethod]
+    [DataRow("1/11/2019 09:54")]
+    [DataRow("18/05/2019 12:14")]
+    [DataRow("28/01/2019 19:23")]
+    public async Task When_RowDatevalid_Then_NoError(string date)
+    {
+        var result = await _CsvValidator.ValidateRows(new Dictionary<string, string>
+        {
+            {MeterReadingCsvValidation.AccountIdHeader , "12"},
+            {MeterReadingCsvValidation.MeterReadingDateTimeHeader , date},
+            {MeterReadingCsvValidation.MeterReadValueHeader , "1,2.0"}
+        });
+
+        Assert.IsNotNull(result);
+        Assert.IsFalse(result.Any());
     }
 }
