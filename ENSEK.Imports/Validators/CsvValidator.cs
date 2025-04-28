abeaee0907f0e866bea717c950c21de32b22212a
Brad Pickering-Dunn <brad@pickering-dunn.com>
2025-04-28 06:35:25 +0100

    Added errors

--- ENSEK.Imports/Validators/CsvValidator.cs
+++ ENSEK.Imports/Validators/CsvValidator.cs
@@ -8,12 +8,9 @@ public class CsvValidator<T> : ICsvValidator<T> where T : class
     {
         var errors = new List<string>();
 
-        if (strings.Length > Validations.MeterReadingCsvValidation.Headers.Count)
-            errors.Add("There are too many columns in the csv.");
-
         foreach (var header in MeterReadingCsvValidation.Headers)
         {
-            if(strings.Any(x => x != header))
+            if(strings.Any(x => x == header))
                 errors.Add($"The header {header} is missing from the csv.");
         }
 
@@ -25,22 +22,22 @@ public class CsvValidator<T> : ICsvValidator<T> where T : class
         var errors = new List<string>();
 
         if (!record.TryGetValue(MeterReadingCsvValidation.MeterReadValueHeader, out var meterReadValue))
-            errors.Add("");
-        else 
-            if(int.TryParse(meterReadValue, out var value))
-                errors.Add("");
+            errors.Add("Can't find value for Meter Read Value");
+        else
+            if (int.TryParse(meterReadValue, out var value))
+            errors.Add($"Can't parse value {value} as Meter Read Value");
 
         if (!record.TryGetValue(MeterReadingCsvValidation.AccountIdHeader, out var accountId))
-            errors.Add("");
+            errors.Add("Can't find value for Account Id");
         else
             if (decimal.TryParse(accountId, out var id))
-                errors.Add("");
+            errors.Add($"Can't parse value {id} as Account Id");
 
         if (!record.TryGetValue(MeterReadingCsvValidation.MeterReadingDateTimeHeader, out var meterReadingDateTime))
-            errors.Add("");
+            errors.Add("Can't find value for Meter Reading DateTime");
         else
             if (DateTime.TryParse(meterReadingDateTime, out var dateTime))
-                errors.Add("");
+                errors.Add($"Can't parse value {dateTime} as Meter Reading DateTime");
 
         return await Task.FromResult(errors);
     }
